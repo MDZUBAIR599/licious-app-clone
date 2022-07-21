85bfae3b5756761db84c2e825ca5b6f8c558fffb
Md Zubair <mdzubair5999@gmail.com>
2022-07-21 22:37:10 +0530

    allroutes

--- licious-clone/src/App.js
+++ licious-clone/src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Components/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
-import Chicken from './Components/Chicken';
-import Fish from './Components/Fish';
-import Mutton from './Components/Mutton';
-import ReadyToCook from './Components/ReadyToCook';
+
+
+import AllRoutes from './Components/AllRoutes';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     <Routes>
-     <Route path="/" element={<Home/>}></Route>
-     <Route path="/chicken" element={<Chicken/>}></Route>
-     
-      <Route path="/fish" element={<Fish/>}></Route>
-
-      <Route path="/mutton" element={<Mutton/>}></Route>
-
-      <Route path="/readytocook" element={<ReadyToCook/>}></Route>
-
-      
-     </Routes>
+     <AllRoutes/>
     </div>
   );
 }
